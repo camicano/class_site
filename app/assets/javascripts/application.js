a2f799ffdf8c6a099e7905334b659cdf60248376
Camila Cano <camilacanog@gmail.com>
2013-11-18 17:40:03 -0500

    css animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,8 +47,11 @@ function ajax(){
 						"width": (width - 40) + 'px',
 						"height": (height - 100) + 'px',
 						"margin-bottom": 50 + 'px',
-						"margin-top": 40 + 'px'
+						"margin-top": 40 + 'px',
+						"left": "10px",
+						"top": "20px"
 					});
+
 
 					var html = '<div class="info">';
 					html += '<h3>Contact Info<i class="icon-times icon-3x"></i></h3>';
@@ -60,31 +63,20 @@ function ajax(){
 					if(student.github){
 						html += '<a href="http://github.com/' + student.github + '" target="blank"><p>' + student.github + '</p></a>';
 					}
-					if(student.twitter){
-						html += '<a href="http://twitter.com/' + student.twitter + '" target="blank"><p>' + student.twitter + '</p></a>';
-					}
-					if(student.bio){
-						html += '<p>' + student.bio + '</p>';
-					}
 					if(student.project_url || student.project_url2 || student.project_url3){
 						html += '<h3>Work</h3>';
 					}
 					if(student.project_url){
 						html += '<a href="' + student.project_url + '" target="blank"><p>' + student.project_url + '</p></a>';
 					}
-					if(student.project_url2){
-						html += '<a href="' + student.project_url2 + '" target="blank"><p>' + student.project_url2 + '</p></a>';
-					}
-					if(student.project_url3){
-						html += '<a href="' + student.project_url3 + '" target="blank"><p>' + student.project_url3 + '</p></a>';
-					}
 
-					html += '<button class="exit-button">Exit</button></div>';
-					
+					html += '<p class="exit-button">Exit</p></div>';
+					$('#' + data.id).addClass('animated fadeInUp');
 					$('#' + data.id).append(html);
 
 					$('.exit-button').on('click', function(e){
 						e.preventDefault();
+						$('#' + data.id).addClass('animated fadeOutDown');
 						$('#container').empty();
 						ajax();
 					});
@@ -99,6 +91,8 @@ function footerOut(){
 	$('#footer').css({
 		"height": 80 + "%"
 	});
+	$('#footer').removeClass('fadeInDown');
+	$('#footer').addClass('animated fadeInUp');
 	$('#footer_content').show();
 }
 
@@ -107,6 +101,8 @@ function footerIn(){
 	$('#footer').css({
 		"height": 70 + "px"
 	});
+	$('#footer').removeClass('fadeInUp');
+	$('#footer').addClass('animated fadeInDown');
 }
 
 $(function(){
